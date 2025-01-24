c95fa21e6af8e637f6a9b63e1cd8bd4a7aea66b6
jvcarpena <jvcarpena2@gmail.com>
2025-01-24 09:41:39 +0800

    Make some changes in games.js instead of using var, used let and const instead.

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,84 +1,78 @@
-var buttonColors = ["red", "blue", "green", "yellow"];
-var gamePattern = [];
-var userClickedPattern = [];
-var started = false;
-var level = 0;
+const buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let started = false;
+let level = 0;
 
-$(document).keydown(function() {
-    if (!started){
-        $("#level-title").text("Level " + level)
+$(document).keydown(function () {
+    if (!started) {
+        $("#level-title").text("Level " + level);
         nextSequence();
         started = true;
     }
 });
 
-$(".btn").on("click", function() {
-    var userChosenColor = $(this).attr("id");
-    //console.log(userChosenColor);
+$(".btn").on("click", function () {
+    const userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
-    //console.log(userClickedPattern);
+
     playSound(userChosenColor);
     animatePress(userChosenColor);
     checkAnswer(userClickedPattern.length - 1);
 });
-
 
 function checkAnswer(currentLevel) {
     if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
         console.log("success");
 
         if (userClickedPattern.length === gamePattern.length) {
-            setTimeout(function() {
+            setTimeout(function () {
                 nextSequence();
             }, 1000);
         }
-
     } else {
         console.log("wrong");
 
         playSound("wrong");
 
         $("body").addClass("game-over");
-        setTimeout(function() {
+        setTimeout(function () {
             $("body").removeClass("game-over");
         }, 200);
 
-        $("#level-title").text("Game Over, Press Any Key to Restart")
+        $("#level-title").text("Game Over, Press Any Key to Restart");
         startOver();
     }
 }
 
-
 function nextSequence() {
-
     userClickedPattern = [];
     level++;
     $("#level-title").text("Level " + level);
 
-    var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColor = buttonColors[randomNumber];
+    const randomNumber = Math.floor(Math.random() * 4);
+    const randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
 
-    $("#" + randomChosenColor).fadeIn(30).fadeOut(30).fadeIn(30);  // to flash..
+    $("#" + randomChosenColor)
+        .fadeIn(30)
+        .fadeOut(30)
+        .fadeIn(30); // to flash
     playSound(randomChosenColor);
-
 }
-
 
 function playSound(colorName) {
-    var audio = new Audio("sounds/" + colorName + ".mp3");
+    const audio = new Audio("sounds/" + colorName + ".mp3");
     audio.play();
 }
-
 
 function animatePress(currentColor) {
     $("#" + currentColor).addClass("pressed");
 
-    setTimeout(function() {
+    setTimeout(function () {
         $("#" + currentColor).removeClass("pressed");
     }, 100);
 }
-
 
 function startOver() {
     level = 0;
